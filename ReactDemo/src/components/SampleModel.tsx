5518ca7b6fb132cd91c0136ebe7e514b8e1dfb19
Austin Huang <gengminghuang@gmail.com>
2021-11-05 15:40:04 +0900

    Holistic working in App.tsx

--- ReactDemo/src/components/SampleModel.tsx
+++ ReactDemo/src/components/SampleModel.tsx
@@ -1,19 +1,18 @@
-import React, { Suspense } from 'react'
-import Avatar from './Avatar'
+import React, { Suspense } from "react";
+import Avatar from "./Avatar";
 
 interface Props {
-  url: string
+  url: string;
 }
 
 export default function SampleModel({ url }: Props) {
-
-  if (url === undefined){
-    url = '../models/AnimeGirl.vrm'
+  if (url === undefined) {
+    url = "../models/AnimeGirl.vrm";
   }
-  
+
   return (
     <Suspense fallback={null}>
       <Avatar url={url} />
     </Suspense>
-  )
+  );
 }
